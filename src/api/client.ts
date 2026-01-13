a23cf5cb14c3d316643108b3545a17af23bedff1
ilovesecu <ilovesecu@gmail.com>
2026-01-14 00:21:17 +0900

    refreshToken으로 AccessToken 발급받기 대기열까지 완료

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,19 @@
 import axios, {type AxiosInstance, type AxiosRequestConfig} from "axios";
 import type {LoginResponse} from "../types/login.ts";
 
+let isRefreshing = false; //현재 재발급인지 체크하는 플래그
+let refreshSubscribers = []; //재발급을 기다리는 요청들의 목록 (대기열)
+
+//새 토큰을 받으면 새 토큰으로 대기요청을 일괄 처리한다.
+const onRefreshed = (accessToken) => {
+    refreshSubscribers.forEach((callback)=>callback(accessToken));
+    refreshSubscribers = [];
+}
+//대기열에 엑세스 토큰 받고 실행할 함수들을 추가해준다.
+const addRefreshSubscriber = (callback) => {
+    refreshSubscribers.push(callback);
+}
+
 export const client:AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 5000, //5초 타임아웃
@@ -35,17 +48,37 @@ client.interceptors.response.use(
         const originalRequest = error.config;
 
         //onRejected - 실패
-        console.log(error);
         if(error.response && error.response.data.code === 'TOKEN_001' && !originalRequest._retry){
-            originalRequest._retry = true; // 무한 루프 방지용 플래그 설정(한번만 시도 후 안되면 포기)
+
             console.error('로그인 필요!');
             try{
+                //이미 다른 요청이 재발급 진행중인지 체크한다. (true:재발급 진행중)
+                if(isRefreshing){
+                    return new Promise((resolve)=>{
+                        addRefreshSubscriber((accessToken)=>{
+                            originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+                            resolve(client(originalRequest)); // 새 토큰으로 재요청 후 결과 반환
+                        })
+                    });
+                }
+
+                //내가 처음으로 401을 받았으면 직접 재발급 시작해야함.
                 console.error('Refresh Proc...');
+                originalRequest._retry = true; // 무한 루프 방지용 플래그 설정(한번만 시도 후 안되면 포기)
+                isRefreshing = true;
+
                 const response = await client.post<LoginResponse>('/auth/reissue');
                 const newAccessToken = response.accessToken;
                 console.log(newAccessToken);
 
+                //재발급 성공!
                 localStorage.setItem('accessToken', newAccessToken);
+                isRefreshing = false;
+
+                //대기열에 있떤 친구들 모두 실행
+                onRefreshed(newAccessToken);
+
+                //내 원래 요청도 새로운 토큰으로 재실행
                 originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
                 return client(originalRequest); //Token 만료 때문에 실패했던 원래 요청을 새 토큰으로 다시 시도한다.
             }catch (refreshError){
